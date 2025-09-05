b555aeb86a1d4a5dfef066f074dd511e21d28d7d
StarleapStudent <starleapstudent@gmail.com>
2025-09-05 22:38:49 +0000

    Week 3 work

--- chapters/03_1_lesson/exercises.py
+++ chapters/03_1_lesson/exercises.py
@@ -15,22 +15,66 @@ def right_justify(input):
 right_justify('Monty')
 
 
-# Do your work for Exercise 1 here
-
-print("Ch 3 Exercise 1: Not implemented") # Delete this line when you write your code!
-
-
-
 print("********** Ch 3 Exercise 2 **********")
 
 # Do your work for Excercise 2 here.
 
-print("Ch 3 Exercise 2: Not implemented") # Delete this line when you write your code!
+def do_twice(f):
+    f()
+    f()
 
+def print_spam():
+    print('spam')
+
+def do_four(f):
+    do_twice(f)
+    do_twice(f)
+
+do_twice(print_spam)
+do_four(print_spam)
 
 
 print("********** Ch 3 Exercise 3 **********")
 
-# Do your work for Exercise 3 here.
+def draw_h_corner():
+    print('+', end=' ')
 
-print("Ch 3 Exercise 3: Not implemented") # Delete this line when you write your code!
+def draw_h_segment():
+    print('-', end=' ')
+
+def draw_h_cell():
+    draw_h_corner()
+    do_four(draw_h_segment)
+
+def end_h_cell():
+    print('+')
+
+def draw_h_lines():
+    do_twice(draw_h_cell)
+    end_h_cell()
+
+def draw_v_end():
+    print('|', end=' ')
+
+def draw_v_segment():
+    print(' ', end=' ')
+
+def draw_v_cell():
+    draw_v_end()
+    do_four(draw_v_segment)
+
+def end_v_cell():
+    print('|')
+
+def draw_v_lines():
+    do_twice(draw_v_cell)
+    end_v_cell()
+    
+def draw_grid():
+    draw_h_lines()
+    do_four(draw_v_lines)
+    draw_h_lines()
+    do_four(draw_v_lines)
+    draw_h_lines()
+
+draw_grid()
